3d355134998eb83aa936ea41ec0f00c595722283
Michael Neustadter <mneustadts@outlook.com>
2020-09-27 08:29:36 -0500

    updated D3

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
 // from data.js
 var tableData = data;
-
-var tableData = data;
-console.log(tableData);
+//console.log(tableData);
 
 // Get a reference to the table body
 var tableBody = d3.select("tablebody");
+
+function getRecords(dt) {
+    var dateFormat = new Date(dt);
