8d3f9b9406ed7903e9aeaa9f625f139f3100de4b
Michael Neustadter <mneustadts@outlook.com>
2020-09-27 10:47:43 -0500

    fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,6 @@
-// from data.js
+  
 var tableData = data;
-//console.log(tableData);
 
-// Get a reference to the table body
-var tableBody = d3.select("tablebody");
 
-function getRecords(dt) {
-    var dateFormat = new Date(dt);
+function appendTable(data) {
+    d3.select("tbody").html("");
