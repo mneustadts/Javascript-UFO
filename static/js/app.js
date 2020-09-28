539b7376fb525a70f952f143b4947ed0d6209ddf
Michael Neustadter <mneustadts@outlook.com>
2020-09-28 10:39:05 -0500

    fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,19 @@
   
 var tableData = data;
+console.log(tableData);
 
-
+// Refer to the table body
 function appendTable(data) {
     d3.select('tbody').html('');
+    // Sighting values for each column
     data.forEach((selection) => {
+        // Append table row `tr` for each UFO Sighting object
         var tableRow = d3.select('tbody').append('tr');
+        // Use values to find each UFO Sighting value
         Object.values(selection).forEach((value) => {
             var tableData = tableRow.append('td');
             tableData.text(value);
         });
     })
 }
+
